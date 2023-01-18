af9368689fa65283656629ec08c4d76dea467e5b
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-01-18 16:24:03 +0100

    arraged the navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      xs: '270px',
+      sm:'650px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   plugins: [],
